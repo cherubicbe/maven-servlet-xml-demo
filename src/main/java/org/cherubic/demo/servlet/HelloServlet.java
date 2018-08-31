87b54f18f2f8d958ea63a41d38e9a929cad2c2f5
cutiechi <chi@cutie.ooo>
2018-08-31 15:06:49 +0800

    feat(servlet): add do get method

--- src/main/java/org/cherubic/demo/servlet/HelloServlet.java
+++ src/main/java/org/cherubic/demo/servlet/HelloServlet.java
@@ -1,7 +1,16 @@
 package org.cherubic.demo.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
-
+    
+    @Override
+    protected void doGet (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        httpServletRequest.getRequestDispatcher("WEB-INF/pages/hello/index.jsp").forward(httpServletRequest, httpServletResponse);
+    }
 }
